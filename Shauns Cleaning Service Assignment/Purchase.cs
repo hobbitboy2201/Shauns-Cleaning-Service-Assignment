a6bfa9040d4ed1032204847ab2968a656799e492
hobbitboy2201 <92788577+hobbitboy2201@users.noreply.github.com>
2022-05-17 11:32:45 +0100

    Created a cleaning purchase class

---
+++ Shauns Cleaning Service Assignment/Purchase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shauns_Cleaning_Service_Assignment
+{
+    public class Purchase
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public double Cost { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Cleaning CreatedBy { get; set; }
+        public bool Open { get; set; }
+
+        public Purchase(string description, double cost, Cleaning createdBy, bool open)
+        {
+            Id = Guid.NewGuid();
+            Description = description;
+            Cost = cost;
+            CreatedOn = DateTime.Now;
+            CreatedBy = createdBy;
+            Open = open;
+        }
+
+        public override string ToString()
+        {
+            return $"{Description}  Created By: {CreatedBy} at {CreatedOn}  Cost: {Cost}";
+        }
+    }
+}
